09dace47e945d963cd2b6142fbfa3df77109bee6
Zhao,Hongbo <hongbo.zhao@intel.com>
2024-09-09 06:34:04 +0000

    just test

--- models/transformer.py
+++ models/transformer.py
@@ -47,8 +47,8 @@ class Transformer(nn.Module):
     def forward(self, src, mask, query_embed, pos_embed):
         # flatten NxCxHxW to HWxNxC
         bs, c, h, w = src.shape
-        src = src.flatten(2).permute(2, 0, 1)
-        pos_embed = pos_embed.flatten(2).permute(2, 0, 1)
+        src = src.flatten(2).reshape(h*w, bs, c)
+        pos_embed = pos_embed.flatten(2).reshape(h*w, bs, c)
         query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)
         mask = mask.flatten(1)
 
@@ -56,7 +56,7 @@ class Transformer(nn.Module):
         memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)
         hs = self.decoder(tgt, memory, memory_key_padding_mask=mask,
                           pos=pos_embed, query_pos=query_embed)
-        return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)
+        return hs.transpose(1, 2), memory.reshape(c, h*w, bs).view(bs, c, h, w)
 
 
 class TransformerEncoder(nn.Module):
